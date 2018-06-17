59b3f30a5eae7353bd1267a3bf81942e3302a2fa
calibr <awcalibr@gmail.com>
2018-06-18 02:54:03 +0300

    add request url to error stack

--- test/server_error.js
+++ test/server_error.js
@@ -45,6 +45,7 @@ describe("Server Error", function() {
     it("should return server error on 5xx http status", function(done) {
       client.get("/", function(err, data, res) {
         err.name.should.equal("ServerError");
+        err.stack.indexOf('http://localhost:' + SERVER_PORT + '/').should.equal(1)
         res.statusCode.should.equal(503);
         done();
       });
@@ -81,6 +82,7 @@ describe("Server Error", function() {
 
     it("should return network error when timeout out", function(done) {
       client.get("/", function(err) {
+        err.stack.indexOf('http://localhost:' + SERVER_PORT + '/').should.equal(1)
         err.name.should.equal("NetworkError");
         err.message.should.equal("ESOCKETTIMEDOUT");
         err.retryInfo.try.should.be.greaterThan(1);
